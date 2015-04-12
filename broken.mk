79675149f78135d6bd938bfbec9796af914f2d3f
blkredstarV <blkredstar5@gmail.com>
2015-04-12 14:47:19 -0500

    Full phone

--- broken.mk
+++ broken.mk
@@ -4,7 +4,7 @@ $(call inherit-product, device/samsung/d2usc/full_d2usc.mk)
 $(call inherit-product, vendor/broken/config/nfc_enhanced.mk)
 
 # Inherit some common Broken stuff.
-$(call inherit-product, vendor/broken/config/common_phone.mk)
+$(call inherit-product, vendor/broken/config/common_full_phone.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=d2usc \
